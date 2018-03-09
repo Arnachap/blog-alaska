4e98f9444918f53830263531d7e29f89eb5d7e30
arnachap <arnaud.chapeleur@gmail.com>
2018-03-09 15:50:36 +0100

    prevent multiple PDO connections

--- Model/Database.php
+++ Model/Database.php
@@ -6,11 +6,15 @@ abstract class Database
     protected static $dbName = "blog-alaska";
     protected static $username = "root";
     protected static $password = "root";
+    protected static $db;
 
     protected function dbConnect()
     {
-        $db = new PDO('mysql:host=' . self::$host . '; dbname=' . self::$dbName . '; charset=utf8', self::$username, self::$password,  array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET lc_time_names="fr_FR",NAMES utf8'));
+        if(!isset($db))
+        {
+            $db = new PDO('mysql:host=' . self::$host . '; dbname=' . self::$dbName . '; charset=utf8', self::$username, self::$password,  array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET lc_time_names="fr_FR",NAMES utf8'));
 
-        return $db;
+            return $db;
+        }
     }
 }
